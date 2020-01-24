30b31f6d666090c7522f85c467d71c90e78cdb56
20171cse0609 <60249737+20171cse0609@users.noreply.github.com>
2020-01-24 13:48:25 +0530

    Add files via upload

---
+++ p4.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int prime(int num)
+{
+int i;
+for(i=2;i<num;i++)
+{
+if(num%i==0)
+return 0;
+}
+return 1;
+}
+void main()
+{
+int m,n,res,i;
+scanf("%d%d",&m,&n);
+for(i=m;i<=n;i++)
+{
+res=prime(i);
+ if(res==1)
+    printf("%d\n",i);
+}
+}
+
+
